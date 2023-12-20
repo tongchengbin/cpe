0a528c33adbffeefe9a9ddff3afaaa7ddc0d49e4
tongcb <tongcb@knownsec.com>
2023-12-20 17:49:50 +0800

    support cpe

--- validate.go
+++ validate.go
@@ -7,7 +7,7 @@ func isCPE23(cpeString string) bool {
 	// 添加 CPE 2.3 版本的验证逻辑
 	// 示例正则表达式
 	//cpe:2.3:a:appserv_open_project:appserv:*:*:*:*:*:*:*:*
-	cpeRegex := regexp.MustCompile(`^cpe:2\.3:([aho]?):[*a-zA-Z0-9._%+-]+:[*a-zA-Z0-9._%+-]+:[*a-zA-Z0-9._%+-]+(?::[*a-zA-Z0-9._%+-]+)*$`)
+	cpeRegex := regexp.MustCompile(`^cpe:2\.3:([aho]?):[\p{Han}*a-zA-Z0-9._%+-]+:[\p{Han}*a-zA-Z0-9._%+-]+:[*a-zA-Z0-9._%+-]+(?::[*a-zA-Z0-9._%+-]+)*$`)
 	return cpeRegex.MatchString(cpeString)
 
 }
